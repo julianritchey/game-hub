6ab3eaa3a789a1d9943b09799f55729e1c78c3a7
julianritchey <julian.ritchey@gmail.com>
2023-07-28 12:33:30 -0400

    Refactor: Entity interfaces

--- app/src/components/PlatformIconList.tsx
+++ app/src/components/PlatformIconList.tsx
@@ -1,16 +1,16 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
 import {
-  FaWindows,
-  FaPlaystation,
-  FaXbox,
   FaApple,
   FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
 } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
-import { BsGlobe } from "react-icons/bs";
-import { Platform } from "../entities/Platform";
-import { HStack, Icon, Text } from "@chakra-ui/react";
-import { IconType } from "react-icons";
+import Platform from "../entities/Platform";
 
 interface Props {
   platforms: Platform[];
